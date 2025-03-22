f78c41f3b13e501ba9dac3fffcdfadc596bd7a1b
MrSodiq <136502782+Sodiqjon1@users.noreply.github.com>
2025-03-22 16:20:02 +0500

    Add files via upload

---
+++ art/art.py
@@ -0,0 +1,36 @@
+import turtle
+from pynput import keyboard
+
+t = turtle.Turtle()
+
+
+def move_forward():
+    t.forward(5)
+
+def move_backward():
+    t.backward(5)
+
+def turn_left():
+    t.left(5)
+
+def turn_right():
+    t.right(5)
+
+def on_press(key):
+    try:
+        if key.char == 'w':
+            move_forward()
+        elif key.char == 's':
+            move_backward()
+        elif key.char == 'a':
+            turn_left()
+        elif key.char == 'd':
+            turn_right()
+    except AttributeError:
+        pass  
+
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
+
+turtle.mainloop()
